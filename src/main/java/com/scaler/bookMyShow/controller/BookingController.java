663c4f1e354aa643cf7900cd71393ce863dd0788
KunalKurve <osheenmanasi@gmail.com>
2026-05-03 03:10:25 +0530

    Git Check

--- src/main/java/com/scaler/bookMyShow/controller/BookingController.java
+++ src/main/java/com/scaler/bookMyShow/controller/BookingController.java
@@ -1,6 +1,6 @@
 package com.scaler.bookMyShow.controller;
 
-import com.scaler.bookMyShow.models.Booking;
+//import com.scaler.bookMyShow.models.Booking;
 import com.scaler.bookMyShow.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
