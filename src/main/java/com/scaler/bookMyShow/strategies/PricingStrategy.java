08c08932e6c55e2bd02927583eb352982df692c4
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-27 22:37:19 +0530

    Strategy for pricing

---
+++ src/main/java/com/scaler/bookMyShow/strategies/PricingStrategy.java
@@ -0,0 +1,11 @@
+package com.scaler.bookMyShow.strategies;
+
+import com.scaler.bookMyShow.models.Show;
+import com.scaler.bookMyShow.models.ShowSeat;
+
+import java.util.List;
+
+public interface PricingStrategy {
+
+    double calculateAmount(List<ShowSeat> savedShowSeats, Show show);
+}
