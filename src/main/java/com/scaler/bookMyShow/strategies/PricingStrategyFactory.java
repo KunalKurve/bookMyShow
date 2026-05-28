f8ed1b5b879749c6e75db04cfa5a5d5df7c9aff6
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-28 18:32:20 +0530

    Factory design for pricing add-ons

---
+++ src/main/java/com/scaler/bookMyShow/strategies/PricingStrategyFactory.java
@@ -0,0 +1,40 @@
+package com.scaler.bookMyShow.strategies;
+
+import com.scaler.bookMyShow.models.Show;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+
+@Component
+public class PricingStrategyFactory {
+
+
+    private final BasePricingStrategy basePricingStrategy;
+
+    @Autowired
+    public PricingStrategyFactory(BasePricingStrategy basePricingStrategy){
+        this.basePricingStrategy = basePricingStrategy;
+    }
+
+    public PricingStrategy getPricingStrategy(Show show){
+
+        PricingStrategy pricingStrategy = basePricingStrategy;
+
+        if(isWeekend(show)){
+            pricingStrategy = new WeekendDecorator(pricingStrategy);
+        }
+
+        return pricingStrategy;
+    }
+
+    public boolean isWeekend(Show show){
+
+        DayOfWeek day = DayOfWeek.of(show.getStart_time().getDay());
+
+        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+
+    }
+
+
+}
