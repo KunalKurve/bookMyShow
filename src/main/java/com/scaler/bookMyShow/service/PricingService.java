ff05316faddb2508f7042b1bbe0137c08a64e13e
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-28 18:28:50 +0530

    Strategy pattern for Pricing along with Service

---
+++ src/main/java/com/scaler/bookMyShow/service/PricingService.java
@@ -0,0 +1,28 @@
+package com.scaler.bookMyShow.service;
+
+import com.scaler.bookMyShow.models.Show;
+import com.scaler.bookMyShow.models.ShowSeat;
+import com.scaler.bookMyShow.strategies.PricingStrategy;
+import com.scaler.bookMyShow.strategies.PricingStrategyFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PricingService {
+
+    private final PricingStrategyFactory pricingStrategyFactory;
+
+    @Autowired
+    public PricingService(PricingStrategyFactory pricingStrategyFactory){
+        this.pricingStrategyFactory = pricingStrategyFactory;
+    }
+
+    public double calculateBookingAmount(List<ShowSeat> showSeats, Show show){
+
+        PricingStrategy pricingStrategy = pricingStrategyFactory.getPricingStrategy(show);
+
+        return pricingStrategy.calculateAmount(showSeats, show);
+    }
+}
