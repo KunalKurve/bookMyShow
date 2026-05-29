9f0e4fe8ddc2ba7351e935d874f863f8cdf29011
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-29 14:31:29 +0530

    applied the Command Design Pattern

--- src/main/java/com/scaler/bookMyShow/BookMyShowApplication.java
+++ src/main/java/com/scaler/bookMyShow/BookMyShowApplication.java
@@ -1,21 +1,46 @@
 package com.scaler.bookMyShow;
 
+import com.scaler.bookMyShow.commands.CommandExecutor;
+import com.scaler.bookMyShow.controller.BookingController;
+import com.scaler.bookMyShow.controller.PaymentController;
 import com.scaler.bookMyShow.controller.UserController;
+import com.scaler.bookMyShow.dto.requests.BookingRequestDto;
+import com.scaler.bookMyShow.dto.requests.PaymentRequestDto;
 import com.scaler.bookMyShow.dto.requests.SignupRequestDto;
+import com.scaler.bookMyShow.dto.responses.BookingResponseDto;
+import com.scaler.bookMyShow.dto.responses.PaymentResponseDto;
 import com.scaler.bookMyShow.dto.responses.SignupResponseDto;
 
+import com.scaler.bookMyShow.models.Booking;
+import com.scaler.bookMyShow.models.Show;
+import com.scaler.bookMyShow.models.enums.PaymentGateway;
+import com.scaler.bookMyShow.models.enums.PaymentMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class BookMyShowApplication implements CommandLineRunner {
 
+//	@Autowired
+//	UserController userController;
+//	@Autowired
+//	BookingController bookingController;
+//	@Autowired
+//	PaymentController paymentController;
+
 	@Autowired
-	UserController userController;
+	CommandExecutor commandExecutor;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
@@ -39,14 +64,58 @@ public class BookMyShowApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		SignupRequestDto requestDto = new SignupRequestDto();
-		requestDto.setName("Virat Kohli");
-		requestDto.setEmail("viratkohli18@zoho.in");
-		requestDto.setPhone("1234567890");
-		requestDto.setPassword("virushkaRocks");
 
-		SignupResponseDto responseDto = userController.signUp(requestDto);
-		System.out.println(responseDto.getUserId());
-		System.out.println(responseDto.getResponseStatus());
+//		SignupRequestDto requestDto = new SignupRequestDto();
+//		requestDto.setName("Virat Kohli");
+//		requestDto.setEmail("viratkohli18@zoho.in");
+//		requestDto.setPhone("1234567890");
+//		requestDto.setPassword("virushkaRocks");
+//
+//		SignupResponseDto responseDto = userController.signUp(requestDto);
+//		System.out.println(responseDto.getUserId());
+//		System.out.println(responseDto.getResponseStatus());
+
+//		BookingRequestDto bookingRequestDto = new BookingRequestDto();
+//		bookingRequestDto.setUserId(responseDto.getUserId());
+//		bookingRequestDto.setShowId(1);
+//		List<Integer> showSeatIds = new ArrayList<>();
+//		showSeatIds.add(1);
+//		showSeatIds.add(2);
+//		showSeatIds.add(3);
+//		showSeatIds.add(4);
+//		bookingRequestDto.setShowSeatIds(showSeatIds);
+//		BookingResponseDto bookingResponseDto = bookingController.bookTicket(bookingRequestDto);
+//
+//		System.out.println(
+//				"Booking created with ID: "
+//						+ bookingResponseDto.getBookingId());
+
+//		System.out.println(
+//				"Please complete payment in 15 minutes");
+//
+//		PaymentRequestDto paymentRequestDto = new PaymentRequestDto();
+//		paymentRequestDto.setBookingId(bookingResponseDto.getBookingId());
+//		paymentRequestDto.setPaymentGateway(PaymentGateway.RAZORPAY);
+//		paymentRequestDto.setPaymentMode(PaymentMode.UPI);
+//		PaymentResponseDto paymentResponseDto = paymentController.makePayment(paymentRequestDto);
+//
+//		System.out.println("Payment successful");
+
+		Files.lines(Paths.get("src/main/resources/test_commands.txt"))
+				.filter(line -> !line.isBlank())
+				.filter(line -> !line.startsWith("#"))
+				.forEach(command -> {
+					try{
+						System.out.println("\nExecuting: " + command);
+						commandExecutor.execute(command);
+
+					}
+					catch(Exception e){
+
+						System.out.println("FAILED -> " + command);
+						System.out.println(e.getMessage());
+					}
+				});
+
 	}
 }
