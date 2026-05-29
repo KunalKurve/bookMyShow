a0ae1eee2ecd3965c99ade56d91f18cac4f8995e
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-29 14:11:55 +0530

    method to findByShowId

--- src/main/java/com/scaler/bookMyShow/repository/ShowSeatRepository.java
+++ src/main/java/com/scaler/bookMyShow/repository/ShowSeatRepository.java
@@ -2,8 +2,21 @@ package com.scaler.bookMyShow.repository;
 
 import com.scaler.bookMyShow.models.ShowSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShowSeatRepository extends JpaRepository<ShowSeat, Integer> {
+    List<ShowSeat> findByShowId(int showId);
+
+//    @Query("""
+//        SELECT ss
+//        FROM ShowSeat ss
+//        WHERE ss.show.id = :showId
+//        """)
+//    List<ShowSeat> findByShowId(@Param("showId") int showId);
 }
+
