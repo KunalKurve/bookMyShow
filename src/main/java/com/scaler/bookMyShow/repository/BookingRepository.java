c4a2efe045eec85cd89f2613ef2c6fc441cacf63
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-29 14:30:45 +0530

    added findByBookingStatus in BookingRepository

--- src/main/java/com/scaler/bookMyShow/repository/BookingRepository.java
+++ src/main/java/com/scaler/bookMyShow/repository/BookingRepository.java
@@ -1,9 +1,13 @@
 package com.scaler.bookMyShow.repository;
 
 import com.scaler.bookMyShow.models.Booking;
+import com.scaler.bookMyShow.models.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    List<Booking> findByBookingStatus(BookingStatus bookingStatus);
 }
